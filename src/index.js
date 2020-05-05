c154c58b6c2bb4de0576ae39994f6e4a3556aa2c
Shubham Kiran Latkar <43436727+shubhamlatkar@users.noreply.github.com>
2020-05-05 22:46:47 +0530

    MOD

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import App from "../src/React-hooks/MyApp"; //react-hooks
-// import App from "./React-redux-thunk/App"; //react-redux-thunk
+import App from "./React-redux-thunk/App"; //react-redux-thunk
 // import App from "../src/React-redux-bootstrap/App"; // react-redux-bootstrap
-import App from "../src/Mentor-on-demand/App";
+// import App from "../src/Mentor-on-demand/App";
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
